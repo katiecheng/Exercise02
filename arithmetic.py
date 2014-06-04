f477f9cf1e18744f8f166f553994f3c33945da64
Hackbright Student <info@hackbrightacademy.com>
2014-06-04 12:37:47 -0700

    Initial Commit

---
+++ arithmetic.py
@@ -0,0 +1,29 @@
+def add(num1, num2):
+    return 10
+
+def subtract(num1, num2):
+    pass
+
+def multiply(num1, num2):
+    pass
+
+def divide(num1, num2):
+    pass
+
+def square(num1):
+    pass
+
+def cube(num1):
+    pass
+
+def power(num1, num2):
+    pass
+
+def mod(num1, num2):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
